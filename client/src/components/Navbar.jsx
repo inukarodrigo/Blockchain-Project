b626da9023c6512a53bab2954c633c3d36679f98
InukaRodrigo <inuka.rodrigo@gmail.com>
2023-07-12 13:51:08 +0530

    Added the login component and created a function that will send an email regarding the transaction.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useRef } from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
-
+import emailjs from "@emailjs/browser";
 import logo from "../../images/logo.png";
 
 const NavBarItem = ({ title, classprops }) => (
@@ -10,20 +10,33 @@ const NavBarItem = ({ title, classprops }) => (
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = React.useState(false);
+  const form = useRef();
 
+  // 1. Output of the AI model needs to be passed to this function
+  // 2. Based on the result of the model (1 or 0), the function has to decide if the email needs to be send or not
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_9d4gy53", "template_r3t69sp", form.current, "wORxbtBe_xIcUzQi7")
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
-      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
-          <NavBarItem key={item + index} title={item} />
-        ))}
-        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
-        </li>
-      </ul>
+      <div className="right-j">
+        <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+          <form ref={form} className="email-container" onSubmit={sendEmail}>
+            <input type="email" name="user_email" placeholder="Enter your Email address" />
+            <button className="btn btn-j">Login</button>
+          </form>
+        </ul>
+      </div>
       <div className="flex relative">
         {!toggleMenu && (
           <HiMenuAlt4 fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
